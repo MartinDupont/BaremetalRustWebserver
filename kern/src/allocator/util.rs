d7bb5dac1d1d44ba2f31deeecadd5a9a0f8f9d37
Martin Dupont <martin.dupont@tngtech.com>
2022-06-03 08:22:34 +0200

    Fixed util function

--- kern/src/allocator/util.rs
+++ kern/src/allocator/util.rs
@@ -6,8 +6,8 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    if (align & 1) != 0 {
-        panic!()
+    if align == 0 || (align & (align - 1)) != 0 {
+        panic!("{} is not a power of 2", align);
     }
     (addr / align) * align
 }
@@ -21,8 +21,11 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 /// Panics if `align` is not a power of 2
 /// or aligning up overflows the address.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    if (align & 1) != 0 {
-        panic!()
+    if align == 0 || (align & (align - 1)) != 0 {
+        panic!("{} is not a power of 2", align);
+    }
+    if addr % align == 0 {
+        return addr
     }
     ((addr / align) + 1) * align
 }
