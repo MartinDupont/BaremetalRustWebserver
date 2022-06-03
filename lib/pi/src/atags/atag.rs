87194193b0293b4134a90f769e2a6f0b1d8a7863
Martin Dupont <martin.dupont@tngtech.com>
2022-06-03 05:39:44 +0200

    Implemented basic methods

--- lib/pi/src/atags/atag.rs
+++ lib/pi/src/atags/atag.rs
@@ -15,18 +15,27 @@ pub enum Atag {
 impl Atag {
     /// Returns `Some` if this is a `Core` ATAG. Otherwise returns `None`.
     pub fn core(self) -> Option<Core> {
-        unimplemented!()
+        match self {
+            Atag::Core(x) => Some(x),
+            _ => None
+        }
     }
 
     /// Returns `Some` if this is a `Mem` ATAG. Otherwise returns `None`.
     pub fn mem(self) -> Option<Mem> {
-        unimplemented!()
+        match self {
+            Atag::Mem(x) => Some(x),
+            _ => None
+        }
     }
 
     /// Returns `Some` with the command line string if this is a `Cmd` ATAG.
     /// Otherwise returns `None`.
     pub fn cmd(self) -> Option<&'static str> {
-        unimplemented!()
+        match self {
+            Atag::Cmd(x) => Some(x),
+            _ => None
+        }
     }
 }
 
