25329fc256f01496c74fe382138a903bea8efa63
Martin Dupont <martin.dupont@tngtech.com>
2022-06-03 05:47:53 +0200

    Almost working "from" implementation

--- lib/pi/src/atags/atag.rs
+++ lib/pi/src/atags/atag.rs
@@ -39,18 +39,17 @@ impl Atag {
     }
 }
 
-// FIXME: Implement `From<&raw::Atag> for `Atag`.
 impl From<&'static raw::Atag> for Atag {
     fn from(atag: &'static raw::Atag) -> Atag {
         // FIXME: Complete the implementation below.
 
         unsafe {
             match (atag.tag, &atag.kind) {
-                (raw::Atag::CORE, &raw::Kind { core }) => unimplemented!(),
-                (raw::Atag::MEM, &raw::Kind { mem }) => unimplemented!(),
-                (raw::Atag::CMDLINE, &raw::Kind { ref cmd }) => unimplemented!(),
-                (raw::Atag::NONE, _) => unimplemented!(),
-                (id, _) => unimplemented!(),
+                (raw::Atag::CORE, &raw::Kind { core }) => Atag::Core(core),
+                (raw::Atag::MEM, &raw::Kind { mem }) => Atag::Mem(mem),
+                (raw::Atag::CMDLINE, &raw::Kind { ref cmd }) => Atag::Cmd("cmd as str"),
+                (raw::Atag::NONE, _) => Atag::None,
+                (id, _) => Atag::Unknown(id),
             }
         }
     }
