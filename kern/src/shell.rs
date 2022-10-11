3f2905691883450b014e983862b2c43790b95185
Martin Dupont <m.dupont@live.com.au>
2022-10-11 11:48:33 +1100

    Added pwd and ls command

--- kern/src/shell.rs
+++ kern/src/shell.rs
@@ -1,7 +1,5 @@
 use shim::io;
-use shim::path::{Path, PathBuf};
-
-use stack_vec::StackVec;
+use shim::path::{Path, PathBuf, Component};
 
 use pi::atags::Atags;
 
@@ -9,10 +7,12 @@ use fat32::traits::FileSystem;
 use fat32::traits::{Dir, Entry};
 
 use crate::console::{kprint, kprintln, CONSOLE};
-use crate::ALLOCATOR;
+use crate::{ALLOCATOR, FILESYSTEM};
 
 use shim::io::Write;
 use shim::io::Read;
+
+use alloc::vec::Vec;
 
 use core::str;
 use core::fmt;
@@ -26,7 +26,7 @@ enum Error {
 
 /// A structure representing a single shell command.
 struct Command<'a> {
-    args: StackVec<'a, &'a str>,
+    args: Vec<&'a str>,
 }
 
 impl<'a> Command<'a> {
@@ -38,9 +38,9 @@ impl<'a> Command<'a> {
     /// If `s` contains no arguments, returns `Error::Empty`. If there are more
     /// arguments than `buf` can hold, returns `Error::TooManyArgs`.
     fn parse(s: &'a str, buf: &'a mut [&'a str]) -> Result<Command<'a>, Error> {
-        let mut args = StackVec::new(buf);
+        let mut args = Vec::new();
         for arg in s.split(' ').filter(|a| !a.is_empty()) {
-            args.push(arg).map_err(|_| Error::TooManyArgs)?;
+            args.push(arg);
         }
 
         if args.is_empty() {
@@ -56,86 +56,169 @@ impl<'a> Command<'a> {
     }
 }
 
-/// Starts a shell using `prefix` as the prefix for each line. This function
-/// never returns.
-pub fn shell(prefix: &str) -> ! {
-    const CMD_LEN: usize = 512;
-    const ARG_LEN: usize = 64;
-    kprintln!();
-    kprintln!("======================================================================");
-    kprintln!("                           Welcome to my OS                           ");
-    kprintln!("======================================================================");
-    kprintln!();
-    loop {
-        let mut cmd_buf = [0u8; CMD_LEN];
-        let mut arg_buf = [""; ARG_LEN];
 
-        kprint!("{}", prefix);
+struct Shell {
+    cwd: PathBuf,
+}
 
-        let mut i = 0;
-        'cmd: loop {
-            if i == CMD_LEN {
-                kprintln!();
-                kprintln!("command length exceeds {}", CMD_LEN);
-                break 'cmd;
-            }
+impl Shell {
+    pub fn new() -> Shell {
+        Shell { cwd: PathBuf::from("/") }
+    }
 
-            let byte = CONSOLE.lock().read_byte();
-            if byte == b'\n' || byte == b'\r' {
-                kprint!("\n");
-                let cmd_result = str::from_utf8(&cmd_buf[0..i]);
-                if let Ok(cmd) = cmd_result {
-                    match Command::parse(cmd, &mut arg_buf) { // enter
-                        Err(Error::Empty) => {}
-                        Err(Error::TooManyArgs) => {
-                            kprintln!("error: too many arguments");
-                        }
-                        Ok(cmd) => {
-                            process_command(cmd);
-                        }
-                    }
+    fn _shell(&self, prefix: &str) -> ! {
+        const CMD_LEN: usize = 512;
+        const ARG_LEN: usize = 64;
+        kprintln!();
+        kprintln!("======================================================================");
+        kprintln!("                           Welcome to my OS                           ");
+        kprintln!("======================================================================");
+        kprintln!();
+        loop {
+            let mut cmd_buf = [0u8; CMD_LEN];
+            let mut arg_buf = [""; ARG_LEN];
+
+            kprint!("{}", prefix);
+
+            let mut i = 0;
+            'cmd: loop {
+                if i == CMD_LEN {
+                    kprintln!();
+                    kprintln!("command length exceeds {}", CMD_LEN);
                     break 'cmd;
+                }
+
+                let byte = CONSOLE.lock().read_byte();
+                if byte == b'\n' || byte == b'\r' {
+                    kprint!("\n");
+                    let cmd_result = str::from_utf8(&cmd_buf[0..i]);
+                    if let Ok(cmd) = cmd_result {
+                        match Command::parse(cmd, &mut arg_buf) { // enter
+                            Err(Error::Empty) => {}
+                            Err(Error::TooManyArgs) => {
+                                kprintln!("error: too many arguments");
+                            }
+                            Ok(cmd) => {
+                                self.process_command(cmd);
+                            }
+                        }
+                        break 'cmd;
+                    } else {
+                        kprintln!("Could not parse input bytes into string");
+                        kprint!("\u{7}");
+                        kprintln!("");
+                        cmd_buf = [0u8; CMD_LEN];
+                        arg_buf = [""; ARG_LEN];
+                    }
+                } else if byte == 8 || byte == 127 { // backspace
+                    if i > 0 {
+                        kprint!("\u{8} \u{8}");
+                        i -= 1
+                    }
                 } else {
-                    kprintln!("Could not parse input bytes into string");
-                    kprint!("\u{7}");
-                    kprintln!("");
-                    cmd_buf = [0u8; CMD_LEN];
-                    arg_buf = [""; ARG_LEN];
+                    cmd_buf[i] = byte;
+                    CONSOLE.lock().write_byte(byte);
+                    i += 1;
                 }
-            } else if byte == 8 || byte == 127 { // backspace
-                if i > 0 {
-                    kprint!("\u{8} \u{8}");
-                    i -= 1
-                }
-            } else {
-                cmd_buf[i] = byte;
-                CONSOLE.lock().write_byte(byte);
-                i += 1;
             }
         }
+    }
+
+
+    fn process_command(&self, cmd: Command) -> Option<()> {
+        let arg1 = cmd.path();
+        match arg1 {
+            "echo" => {
+                let mut count = 0;
+                for arg in cmd.args {
+                    if count > 0 {
+                        kprint!("{} ", arg);
+                    }
+                    count += 1
+                }
+                kprint!("\n");
+            }
+            "pwd" => {
+                kprintln!("{}", self.cwd.to_str().unwrap());
+            }
+            "ls" => { self.ls(cmd.args) }
+            "" => {
+                kprintln!();
+            }
+            _ => {
+                kprintln!("Unknown command: {}", arg1);
+            }
+        };
+        return Some(());
+    }
+
+
+    fn ls(&self, mut args: Vec<&str>) {
+        let mut display_hidden = false;
+        args.remove(0); // Remove the ls command
+        if args.len() > 0  {
+            let arg = args.remove(0);
+            if arg == "-a" {
+                display_hidden = true;
+            } else {
+                args.insert(0, arg);
+            }
+        }
+
+        let ls_dir = |path: &PathBuf| {
+            match FILESYSTEM.open(path) {
+                Ok(entry) => match entry.as_dir() {
+                    Some(dir) => {
+                        match dir.entries() {
+                            Ok(entries) => {
+                                for entry in entries {
+                                    if display_hidden || !entry.metadata().attributes.hidden() {
+                                        kprintln!("{}", entry.name());
+                                    }
+                                }
+                            }
+                            Err(_) => kprintln!("Cannot open directory {}", path.to_str().unwrap()),
+                        }
+                    }
+                    None => kprintln!("{}", entry.name()),
+                }
+                Err(_) => kprintln!("Cannot open directory {}", path.to_str().unwrap()),
+            };
+        };
+
+        if args.len() == 0 {
+            // ls in cwd
+            ls_dir(&self.cwd);
+        } else {
+            // ls each argument
+            for arg in args {
+                ls_dir(&self.get_entry(arg));
+            }
+        }
+
+    }
+
+    // Gets the entries identified by the given path.
+    fn get_entry(&self, path: &str) -> PathBuf {
+        let mut curr = self.cwd.clone();
+        let path = PathBuf::from(path);
+
+        for component in path.components() {
+            match component {
+                Component::RootDir => curr = PathBuf::from("/"),
+                Component::ParentDir => { curr.pop(); },
+                Component::Normal(entry) => curr.push(entry),
+                _ => (), // Nothing to do for `Prefix` or `CurDir`
+            }
+        }
+        curr
     }
 }
 
 
-fn process_command(cmd: Command) -> Option<()> {
-    let arg1 = cmd.path();
-    match arg1 {
-        "echo" => {
-            let mut count = 0;
-            for arg in cmd.args {
-                if count > 0 {
-                    kprint!("{} ", arg);
-                }
-                count += 1
-            }
-            kprint!("\n");
-        }
-        "" => {
-            kprintln!();
-        }
-        _ => {
-            kprintln!("Unknown command: {}", arg1);
-        }
-    };
-    return Some(());
+/// Starts a shell using `prefix` as the prefix for each line. This function
+/// never returns.
+pub fn shell(prefix: &str) -> ! {
+    let the_shell = Shell::new();
+    the_shell._shell(prefix)
 }
